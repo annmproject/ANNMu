dfce17b5b2884add1158d95c94a5835e30b72b07
Adam Novak <annmproject@protonmail.com>
2022-02-24 11:38:21 +0100

    Translations

---
+++ include/ANNMu/translations/cs.hpp
@@ -0,0 +1,98 @@
+/*
+ * 
+ * This file is part of the ANNMu project.
+ * 
+ * Copyright (c) 2021-2022 Adam Novak <annmproject@protonmail.com>
+ *
+ * This project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project. If not, see https://www.gnu.org/licenses/.
+ *
+ * You can be released from the requirements of the license by obtaining
+ * a commercial license. Getting such a license is mandatory as soon as you
+ * develop commercial activities involving the ANNMu source code without
+ * disclosing the source code of your own applications.
+ *
+ */
+
+#ifndef ANNMu_TRANSLATIONS_CS_HPP
+#define ANNMu_TRANSLATIONS_CS_HPP
+
+namespace annmu {
+
+    namespace translations {
+
+        namespace cs {
+            
+            static const char * const WORKING_HOURS = "Pracovní doba";
+            static const char * const OPENING_HORUS = "Otevírací doba";
+
+            static const char * const MONDAY = "Pondělí";
+            static const char * const TUESDAY = "Úterý";
+            static const char * const WEDNESDAY = "Středa";
+            static const char * const THURSDAY = "Čtvrtek";
+            static const char * const FRIDAY = "Pátek";
+            static const char * const SATURDAY = "Sobota";
+            static const char * const SUNDAY = "Neděle";
+
+            static const char * const MON = "Po";
+            static const char * const TUES = "Út";
+            static const char * const WED = "St";
+            static const char * const THUR = "Čt";
+            static const char * const FRI = "Pá";
+            static const char * const SAT = "So";
+            static const char * const SUN = "Ne";
+
+            static const char * const NAME = "Jméno";
+            static const char * const SURNAME = "Příjmení";
+            static const char * const ADDRESS = "Adresa";
+            static const char * const CITY = "Obec";
+            static const char * const STREET = "Ulice";
+            static const char * const HOUSE_NUMBER = "Číslo popisné";
+            static const char * const PHONE = "Telefon";
+            static const char * const EMAIL = "E-mail";
+
+            static const char * const SEND = "Odeslat";
+            static const char * const BACK = "Zpět";
+            static const char * const I_AGREE = "Souhlasím";
+            static const char * const LOG_IN = "Přihlásit se";
+            static const char * const LOG_OUT = "Odhlásit se";
+            static const char * const REGISTER = "Registrovat se";
+            static const char * const REGISTRATION = "Registrace";
+            static const char * const SAVE = "Uložit";
+            static const char * const NEXT = "Další";
+            static const char * const READ_MORE = "Číst více";
+            static const char * const MORE_INFO = "Více informací";
+            static const char * const EDIT = "Upravit";
+            static const char * const CREATE = "Vytvořit";
+            static const char * const NEW = "Nový";
+
+            static const char * const MENU = "Menu";
+            static const char * const CONTACT = "Kontakt";
+            static const char * const ABOUT_US = "O nás";
+            static const char * const HOMEPAGE = "Hlavní strana";
+            static const char * const BLOG = "Blog";
+            static const char * const PRIVACY_POLICY = "Zásady ochrany osobních údajů";
+
+            static const char * const CZECH = "Čeština";
+            static const char * const ENGLISH = "Angličtina";
+            static const char * const DEUTSCH = "Němčina";
+            static const char * const POLAND = "Polština";
+            static const char * const RUSSIAN = "Ruština";
+
+        }
+
+    }
+
+}
+
+#endif
---
+++ include/ANNMu/translations/en.hpp
@@ -0,0 +1,98 @@
+/*
+ * 
+ * This file is part of the ANNMu project.
+ * 
+ * Copyright (c) 2021-2022 Adam Novak <annmproject@protonmail.com>
+ *
+ * This project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project. If not, see https://www.gnu.org/licenses/.
+ *
+ * You can be released from the requirements of the license by obtaining
+ * a commercial license. Getting such a license is mandatory as soon as you
+ * develop commercial activities involving the ANNMu source code without
+ * disclosing the source code of your own applications.
+ *
+ */
+
+#ifndef ANNMu_TRANSLATIONS_EN_HPP
+#define ANNMu_TRANSLATIONS_EN_HPP
+
+namespace annmu {
+
+    namespace translations {
+
+        namespace en {
+            
+            static const char * const WORKING_HOURS = "Working hours";
+            static const char * const OPENING_HORUS = "Opening hours";
+
+            static const char * const MONDAY = "Monday";
+            static const char * const TUESDAY = "Tuesday";
+            static const char * const WEDNESDAY = "Wednesday";
+            static const char * const THURSDAY = "Thursday";
+            static const char * const FRIDAY = "Friday";
+            static const char * const SATURDAY = "Saturday";
+            static const char * const SUNDAY = "Sunday";
+
+            static const char * const MON = "Mon";
+            static const char * const TUES = "Tue";
+            static const char * const WED = "Wed";
+            static const char * const THUR = "Thur";
+            static const char * const FRI = "Fri";
+            static const char * const SAT = "Sat";
+            static const char * const SUN = "Sun";
+
+            static const char * const NAME = "Name";
+            static const char * const SURNAME = "Surname";
+            static const char * const ADDRESS = "Address";
+            static const char * const CITY = "City";
+            static const char * const STREET = "Street";
+            static const char * const HOUSE_NUMBER = "House number";
+            static const char * const PHONE = "PHONE";
+            static const char * const EMAIL = "E-mail";
+
+            static const char * const SEND = "Send";
+            static const char * const BACK = "Back";
+            static const char * const I_AGREE = "I agree";
+            static const char * const LOG_IN = "Log in";
+            static const char * const LOG_OUT = "Log out";
+            static const char * const REGISTER = "Register";
+            static const char * const REGISTRATION = "Registration";
+            static const char * const SAVE = "Save";
+            static const char * const NEXT = "Next";
+            static const char * const READ_MORE = "Read more";
+            static const char * const MORE_INFO = "More information";
+            static const char * const EDIT = "Edit";
+            static const char * const CREATE = "Create";
+            static const char * const NEW = "New";
+
+            static const char * const MENU = "Menu";
+            static const char * const CONTACT = "Contact";
+            static const char * const ABOUT_US = "About us";
+            static const char * const HOMEPAGE = "Homepage";
+            static const char * const BLOG = "Blog";
+            static const char * const PRIVACY_POLICY = "Privacy policy";
+
+            static const char * const CZECH = "Czech";
+            static const char * const ENGLISH = "English";
+            static const char * const DEUTSCH = "Deutsch";
+            static const char * const POLAND = "Poland";
+            static const char * const RUSSIAN = "Russian";
+
+        }
+
+    }
+
+}
+
+#endif
